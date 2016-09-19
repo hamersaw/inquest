e27c3639490ce4f70290622c4683b48d05f1d04b
Dan Rammer <hamersaw@gmail.com>
2016-09-19 15:10:36 -0600

    generalized some request/reply creation

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,6 +1,8 @@
 extern crate inquest;
 extern crate grpc;
 
+use std::collections::HashMap;
+
 use grpc::result::GrpcResult;
 
 use inquest::inquest_pb::{ScheduleProbeRequest, ScheduleProbeReply};
@@ -15,22 +17,45 @@ fn main() {
 }
 
 struct InquestImpl {
-        
+    probe_map: HashMap<String, ScheduleProbeRequest>,            
 }
 
 impl InquestImpl {
     fn new() -> InquestImpl {
         InquestImpl {
-
+            probe_map: HashMap::new(),
         }
+    }
+
+    pub fn contains_probe(&self, probe_id: &str) -> bool {
+        self.probe_map.contains_key(probe_id)
     }
 }
 
 impl Inquest for InquestImpl {
     fn ScheduleProbe(&self, request: ScheduleProbeRequest) -> GrpcResult<ScheduleProbeReply> {
-        let mut reply = ScheduleProbeReply::new();
-        reply.set_success(false);
-        reply.set_error_msg("Unimplemented".to_string());
-        Ok(reply)
+        //check for field 'probe'
+        if !request.has_probe() {
+            return Ok(inquest::schedule_probe_reply_failure("request field probe is empty"));
+        }
+
+        let probe = request.get_probe();
+
+        //check for field 'probe_id'
+        if !probe.has_probe_id() {
+            return Ok(inquest::schedule_probe_reply_failure("request field probe_id is empty"));
+        }
+
+        let probe_id = probe.get_probe_id();
+
+        //check if probe already exists
+        if self.contains_probe(probe_id) {
+            return Ok(inquest::schedule_probe_reply_failure("probe id already exists"));
+        }
+
+        //add probe to probe map
+        //self.probe_map.insert(probe_id.to_owned(), request);
+
+        Ok(inquest::schedule_probe_reply_success())
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,18 @@ extern crate futures_cpupool;
 
 pub mod inquest_pb;
 pub mod inquest_pb_grpc;
+
+use inquest_pb::{ScheduleProbeRequest, ScheduleProbeReply};
+
+pub fn schedule_probe_reply_success() -> ScheduleProbeReply {
+    let mut reply = ScheduleProbeReply::new();
+    reply.set_success(true);
+    reply
+}
+
+pub fn schedule_probe_reply_failure(msg: &str) -> ScheduleProbeReply {
+    let mut reply = ScheduleProbeReply::new();
+    reply.set_success(false);
+    reply.set_error_msg(msg.to_owned());
+    reply
+}
