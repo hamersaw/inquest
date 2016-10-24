480a09c5068bc86e055a3a0040b90136381ca3f4
Dan Rammer <hamersaw@gmail.com>
2016-10-24 09:51:00 -0600

    added variable to hold max number of domains to schedule.

--- scripts/schedule-top-alexa.sh
+++ scripts/schedule-top-alexa.sh
@@ -2,6 +2,7 @@
 INQUISITOR_BIN="/home/hamersaw/Development/rust/inquest/target/debug/inquisitor"
 IP_ADDRESS="127.0.0.1"
 PORT="12289"
+MAX_COUNT=10000
 TMP_DIR="/tmp"
 
 #download alexa top 1 million domains
@@ -9,13 +10,22 @@ wget -O $TMP_DIR/top-1m.csv.zip http://s3.amazonaws.com/alexa-static/top-1m.csv.
 unzip -p $TMP_DIR/top-1m.csv.zip > $TMP_DIR/top-1m.csv
 
 #read file
-while read LINE; do
+COUNT=0
+while read LINE
+do
     #parse domain
     DOMAIN=`echo $LINE | cut -f 2 -d ','`
 
     #TODO specify ip address and port of configuration server
     echo "scheduling $DOMAIN"
     $INQUISITOR_BIN schedule $DOMAIN --https $DOMAIN --follow --interval=3600
+
+    #increment counter
+    COUNT=$[COUNT+1]
+    if [ $COUNT == $MAX_COUNT ];
+    then
+        break
+    fi
 done < $TMP_DIR/top-1m.csv
 
 rm $TMP_DIR/top-1m.csv*
